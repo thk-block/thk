906b2b1733d45bcf3c21051c9e37dc102fe42c0b
thk-block <50817352+thk-block@users.noreply.github.com>
2021-11-17 11:20:33 +0900

    更新 main.blocks, main.ts, thk_neopixel.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-
+let strip: neopixel.Strip = null
+basic.forever(function () {
+    strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+})
