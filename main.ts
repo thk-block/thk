c0eabdfbf7068a4ccbe6af70f11a91900c8d274a
thk-block <50817352+thk-block@users.noreply.github.com>
2021-11-23 11:59:38 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,1 @@
-THK.thk_kyouzai_setup()
-basic.forever(function () {
-    if (THK.is_getDigitalSensor(THK.SensorS.P1)) {
-        THK.thk_servo(THK.Servos.Motor1, 0)
-        THK.thk_NeoPixel_Light(NeoPixelColors.Red)
-    }
-})
-basic.forever(function () {
-    if (THK.is_getDigitalSensor(THK.SensorS.P13)) {
-        THK.thk_servo(THK.Servos.Motor1, 46)
-    }
-})
+
